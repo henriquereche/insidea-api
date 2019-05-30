9ccb551e55a40c62c230b4852570b5d03ba82273
Henrique Reche <henriquenreche@gmail.com>
2019-05-29 22:55:44 -0300

    Dotenv environment settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const development = process.env.NODE_ENV !== 'production';
+  if (development) {
+    dotenv.config();
+  }
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
